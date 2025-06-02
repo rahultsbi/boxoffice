0c06bb412760cf63281d61d5083a11a979f13b0a
rahultsbi <rahulkumar@tsbi.in>
2025-06-02 10:29:23 +0530

    adeednewToday1

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -726,7 +726,7 @@
 //         }
 //     }
 // };
-export const API_BASE_URL = 'http://127.0.0.1:8001';
+export const API_BASE_URL = 'https://box-office-tool-backend.onrender.com';
 
 export const apiService = {
     // Get auth token from localStorage with fallback
